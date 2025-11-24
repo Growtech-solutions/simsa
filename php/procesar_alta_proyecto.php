c396a5068a79420b7d247058b2e746b48b43fbba
Growtech-solutions <rafael@growtech-solutions>
2025-11-24 17:56:06 +0000

    actualizacion_correos

--- php/procesar_alta_proyecto.php
+++ php/procesar_alta_proyecto.php
@@ -56,7 +56,7 @@ if ($resultado && $resultado->num_rows > 0) {
             $mail->Host = 'smtp.gmail.com'; // Servidor SMTP de Google
             $mail->SMTPAuth = true;
             $mail->Username = 'rafael@growtech-solutions.com.mx'; // Alias o dirección principal
-            $mail->Password = 'hnju vixi pstb zfcx'; // Clave de aplicación generada
+            $mail->Password = 'qlld nvlm amig hgab'; // Clave de aplicación generada
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
             $mail->Port = 465;
 
